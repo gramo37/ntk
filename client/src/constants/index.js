6030a05d2768fbdce7645da597ef5e0dc65b815c
LAPTOP29\Prasanna <prasanna.gramopadhye@optanium.com>
2023-10-07 17:19:29 +0530

    First commit

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,2 +1,4 @@
-export const host = "http://68.183.93.128:5000";
+// export const host = "api";
+export const host = "http://api:5000";
+// export const host = "http://68.183.93.128:5000";
 // export const host = "http://localhost:5000";
