e96b3fed4a0f6f732fd97ac171e3ca8aa25509b7
LAPTOP29\Prasanna <prasanna.gramopadhye@optanium.com>
2023-10-08 12:26:24 +0530

    Small changes

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,5 +1,5 @@
 /// export const host = "api";
-export const host = `${process.env.REACT_APP_API_URL}:5000`;
+export const host = process.env.REACT_APP_API_URL;
 // export const host = `http://${process.env.REACT_APP_HOST}:5000`;
 // export const host = "http://68.183.93.128:5000";
 // export const host = "http://localhost:5000";
